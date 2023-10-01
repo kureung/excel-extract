16c3cc90a3c63257d64a5f0768e25c58433dcf88
kureungkureung <terry9611@naver.com>
2023-10-01 15:57:55 +0900

    refactor(CellIndexName): 클래스 접근 지시자 default로 수정

--- src/main/java/io/github/kureung/common/CellIndexName.java
+++ src/main/java/io/github/kureung/common/CellIndexName.java
@@ -4,7 +4,7 @@ import org.apache.poi.ss.util.CellReference;
 
 import java.util.regex.Pattern;
 
-public class CellIndexName {
+class CellIndexName {
     private final String value;
 
     public CellIndexName(final String value) {
