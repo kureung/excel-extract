79adb71f1c1fc420a54da47ca4e5de66ad002583
kureungkureung <terry9611@naver.com>
2023-10-03 02:47:49 +0900

    refactor(XlsxExtractor): 메소드 순서 정리

--- src/main/java/io/github/kureung/xlsx/XlsxExtractor.java
+++ src/main/java/io/github/kureung/xlsx/XlsxExtractor.java
@@ -19,25 +19,25 @@ public class XlsxExtractor {
         this.file = file;
     }
 
-    private <T> Sheet validSheet(Class<T> clazz) {
-//        hasAnnotation ?
-        if (!clazz.isAnnotationPresent(TableExtract.class)) {
-            throw new IllegalStateException("need CellExtractionTable annotation");
-        }
+    public <T> List<T> execute(Class<T> clazz) {
+        List<Row> rows = tableRows(clazz);
+        verify(clazz, rows);
+        List<T> result = new ArrayList<>();
 
-        TableExtract table = clazz.getAnnotation(TableExtract.class);
-        int sheetIndex = table.sheetNumber();
-        Sheet sheet = sheet(sheetIndex);
-
-//         verify sheetName
-        if (table.verifySheetName()) {
-            String sheetName = table.sheetName();
-            if (!sheetName.equals(sheet.getName())) {
-                String message = String.format("시트 이름 불일치. expected=%s, actual=%s", sheetName, sheet.getName());
-                throw new IllegalStateException(message);
+        Field[] fields = clazz.getDeclaredFields();
+        for (Row row : rows) {
+            T instance = newInstance(clazz);
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(CellExtract.class)) {
+                    CellExtract extractionCondition = field.getAnnotation(CellExtract.class);
+                    int x = xCoordinate(extractionCondition.firstDataIndex());
+                    Cell cell = row.getCell(x);
+                    injectField(field, instance, cell.getText());
+                }
             }
+            result.add(instance);
         }
-        return sheet;
+        return result;
     }
 
     private <T> List<Row> tableRows(Class<T> clazz) {
@@ -67,33 +67,33 @@ public class XlsxExtractor {
         return result;
     }
 
+    private <T> Sheet validSheet(Class<T> clazz) {
+//        hasAnnotation ?
+        if (!clazz.isAnnotationPresent(TableExtract.class)) {
+            throw new IllegalStateException("need CellExtractionTable annotation");
+        }
+
+        TableExtract table = clazz.getAnnotation(TableExtract.class);
+        int sheetIndex = table.sheetNumber();
+        Sheet sheet = sheet(sheetIndex);
+
+//         verify sheetName
+        if (table.verifySheetName()) {
+            String sheetName = table.sheetName();
+            if (!sheetName.equals(sheet.getName())) {
+                String message = String.format("시트 이름 불일치. expected=%s, actual=%s", sheetName, sheet.getName());
+                throw new IllegalStateException(message);
+            }
+        }
+        return sheet;
+    }
+
     private List<Row> validRows(Sheet sheet) {
         try {
             return sheet.read();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public <T> List<T> execute(Class<T> clazz) {
-        List<Row> rows = tableRows(clazz);
-        verify(clazz, rows);
-        List<T> result = new ArrayList<>();
-
-        Field[] fields = clazz.getDeclaredFields();
-        for (Row row : rows) {
-            T instance = newInstance(clazz);
-            for (Field field : fields) {
-                if (field.isAnnotationPresent(CellExtract.class)) {
-                    CellExtract extractionCondition = field.getAnnotation(CellExtract.class);
-                    int x = xCoordinate(extractionCondition.firstDataIndex());
-                    Cell cell = row.getCell(x);
-                    injectField(field, instance, cell.getText());
-                }
-            }
-            result.add(instance);
-        }
-        return result;
     }
 
     private int xCoordinate(String cellIndexName) {
@@ -130,7 +130,6 @@ public class XlsxExtractor {
 
     private <T> void verify(Class<T> clazz, List<Row> rows) {
         Field[] fields = clazz.getDeclaredFields();
-
         for (Field field : fields) {
             if (field.isAnnotationPresent(CellExtract.class)) {
                 CellExtract extractionCondition = field.getAnnotation(CellExtract.class);
