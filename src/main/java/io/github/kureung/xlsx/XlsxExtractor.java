7f2a22f85fda06bc6b75c69460bef8f8b77bf779
kureungkureung <terry9611@naver.com>
2023-10-03 19:50:21 +0900

    feat(XlsxExtractor): xlsx 엑셀 추출기

--- src/main/java/io/github/kureung/xlsx/XlsxExtractor.java
+++ src/main/java/io/github/kureung/xlsx/XlsxExtractor.java
@@ -2,15 +2,13 @@ package io.github.kureung.xlsx;
 
 import io.github.kureung.common.CellExtract;
 import io.github.kureung.common.CellIndexName;
-import io.github.kureung.common.TableExtract;
-import org.dhatim.fastexcel.reader.*;
+import org.dhatim.fastexcel.reader.Cell;
+import org.dhatim.fastexcel.reader.Row;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class XlsxExtractor {
     private final File file;
@@ -20,9 +18,20 @@ public class XlsxExtractor {
     }
 
     public <T> List<T> execute(Class<T> clazz) {
-        List<Row> rows = tableRows(clazz);
+        List<Row> rows = rows(file, clazz);
         verifyCategoryValue(clazz, rows);
+        return extract(clazz, rows);
+    }
 
+    private <T> void verifyCategoryValue(Class<T> clazz, List<Row> rows) {
+        new XlsxCategoryValidator<>(clazz, rows).execute();
+    }
+
+    private <T> List<Row> rows(File file, Class<T> clazz) {
+        return new XlsxTableExtractValidator<>(file, clazz).validRows();
+    }
+
+    private <T> List<T> extract(final Class<T> clazz, final List<Row> rows) {
         List<T> result = new ArrayList<>();
 
         Field[] fields = clazz.getDeclaredFields();
@@ -41,79 +50,9 @@ public class XlsxExtractor {
         return result;
     }
 
-    private <T> void verifyCategoryValue(Class<T> clazz, List<Row> rows) {
-        new XlsxCategoryValidator<>(clazz, rows).execute();
-    }
-
-    private <T> List<Row> tableRows(Class<T> clazz) {
-        Sheet sheet = validSheet(clazz);
-        TableExtract table = clazz.getAnnotation(TableExtract.class);
-        int firstValidDataRowNumber = table.firstDataRowIndex() + 1;
-        List<Row> collect = validRows(sheet).stream()
-                .filter(row -> row.getRowNum() >= firstValidDataRowNumber)
-                .collect(Collectors.toList());
-
-        List<Row> result = new ArrayList<>();
-        for (Row row : collect) {
-            if (table.terminateCondition().columNumber() >= 0) {
-                TableExtract.Entry entry = table.terminateCondition();
-                int columNumber = entry.columNumber();
-                if (entry.isNull()) {
-                    if (!row.hasCell(columNumber) || row.getCell(columNumber).getType() == CellType.EMPTY) {
-                        return result;
-                    }
-                }
-                if(row.getCell(columNumber).getText().equals(entry.value())) {
-                    return result;
-                }
-            }
-            result.add(row);
-        }
-        return result;
-    }
-
-    private <T> Sheet validSheet(Class<T> clazz) {
-//        hasAnnotation ?
-        if (!clazz.isAnnotationPresent(TableExtract.class)) {
-            throw new IllegalStateException("need CellExtractionTable annotation");
-        }
-
-        TableExtract table = clazz.getAnnotation(TableExtract.class);
-        int sheetIndex = table.sheetNumber();
-        Sheet sheet = sheet(sheetIndex);
-
-//         verify sheetName
-        if (table.verifySheetName()) {
-            String sheetName = table.sheetName();
-            if (!sheetName.equals(sheet.getName())) {
-                String message = String.format("시트 이름 불일치. expected=%s, actual=%s", sheetName, sheet.getName());
-                throw new IllegalStateException(message);
-            }
-        }
-        return sheet;
-    }
-
-    private List<Row> validRows(Sheet sheet) {
-        try {
-            return sheet.read();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private int xCoordinate(String cellIndexName) {
         return new CellIndexName(cellIndexName)
                 .xCoordinate();
-    }
-
-    private Sheet sheet(int sheetPage) {
-        try {
-            return new ReadableWorkbook(file)
-                    .getSheet(sheetPage)
-                    .orElseThrow(() -> new IllegalStateException("not found sheet"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private <T> T newInstance(Class<T> clazz) {
