852663447658a2b584c8182c1401061e789b23ec
kureungkureung <terry9611@naver.com>
2023-10-03 12:47:11 +0900

    feat(CategoryValidator): 카테고리 검증기 테스트

--- src/main/java/io/github/kureung/xlsx/CategoryValidator.java
+++ src/main/java/io/github/kureung/xlsx/CategoryValidator.java
@@ -8,12 +8,12 @@ import org.dhatim.fastexcel.reader.Row;
 import java.lang.reflect.Field;
 import java.util.List;
 
-class CategoryValidator <T> {
+class CategoryValidator<T> {
 
     private final Class<T> clazz;
     private final List<Row> rows;
 
-    CategoryValidator(final Class<T> clazz, final List<Row> rows) {
+    CategoryValidator(Class<T> clazz, List<Row> rows) {
         this.clazz = clazz;
         this.rows = rows;
     }
@@ -21,40 +21,40 @@ class CategoryValidator <T> {
     void execute() {
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
-            verifyValue(field);
+            checkHasCellExtractAnnotation(field);
         }
     }
 
-    private void verifyValue(Field field) {
+    private void checkHasCellExtractAnnotation(Field field) {
         if (field.isAnnotationPresent(CellExtract.class)) {
             CellExtract extractionCondition = field.getAnnotation(CellExtract.class);
-            extracted(extractionCondition);
+            checkHasVerifyList(extractionCondition);
         }
     }
 
-    private void extracted(CellExtract extractionCondition) {
+    private void checkHasVerifyList(CellExtract extractionCondition) {
         if (extractionCondition.verifyCellCategory()) {
             String categoryIndexName = extractionCondition.categoryIndex();
-            String cellValue = extractionCondition.categoryValue();
             CellIndexName cellIndexName = new CellIndexName(categoryIndexName);
-            extracted2(cellIndexName, cellValue);
+            String cellValue = extractionCondition.categoryValue();
+            rowTraversal(cellIndexName, cellValue);
         }
     }
 
-    private void extracted2(CellIndexName cellIndexName, String cellValue) {
+    private void rowTraversal(CellIndexName cellIndexName, String cellValue) {
         for (Row row : rows) {
-            extracted3(cellIndexName, cellValue, row);
+            verifyRowNumber(cellIndexName, cellValue, row);
         }
     }
 
-    private void extracted3(CellIndexName cellIndexName, String cellValue, Row row) {
+    private void verifyRowNumber(CellIndexName cellIndexName, String cellValue, Row row) {
         if (row.getRowNum() == cellIndexName.yCoordinate() + 1) {
             Cell cell = row.getCell(cellIndexName.xCoordinate());
-            extracted4(cellValue, cell);
+            verifyValue(cellValue, cell);
         }
     }
 
-    private void extracted4(String cellValue, Cell cell) {
+    private void verifyValue(String cellValue, Cell cell) {
         if (!cellValue.equals(cell.getText())) {
             String message = String.format("셀 값 검증 실패. expected=%s, actual=%s", cellValue, cell.getText());
             throw new IllegalStateException(message);
---
+++ src/test/java/io/github/kureung/xlsx/CategoryValidatorTest.java
@@ -0,0 +1,47 @@
+package io.github.kureung.xlsx;
+
+import io.github.kureung.common.CellExtract;
+import org.dhatim.fastexcel.reader.ReadableWorkbook;
+import org.dhatim.fastexcel.reader.Row;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CategoryValidatorTest {
+    @Test
+    void validCategory_is_not_throw_exception() throws IOException {
+        CategoryValidator<ValidCategorySample> sut = new CategoryValidator<>(ValidCategorySample.class, rows());
+
+        assertThatCode(() -> sut.execute())
+                .doesNotThrowAnyException();
+    }
+
+    List<Row> rows() throws IOException {
+        File xlsx = new File("src/test/java/io/github/kureung/xlsx/xlsx_sample.xlsx");
+        return new ReadableWorkbook(xlsx)
+                .getSheet(0).orElseThrow(() -> new IllegalArgumentException("not found sheet"))
+                .read();
+    }
+
+    private static class ValidCategorySample {
+        @CellExtract(categoryIndex = "B3", categoryValue = "성명", firstDataIndex = "B4")
+        private String name;
+    }
+
+    @Test
+    void invalidCategory_is_throw_exception() throws IOException {
+        CategoryValidator<InvalidCategorySample> sut = new CategoryValidator<>(InvalidCategorySample.class, rows());
+        assertThatThrownBy(() -> sut.execute())
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    private static class InvalidCategorySample {
+        @CellExtract(categoryIndex = "B3", categoryValue = "성명1", firstDataIndex = "B4")
+        private String name;
+    }
+}
